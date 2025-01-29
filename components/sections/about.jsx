4304cda5d711fc856e67135719b51dfd1e79b82b
ismail-jr <ismail.jibriel.jr@gmail.com>
2025-01-29 22:14:48 +0000

    more commit

--- components/sections/about.jsx
+++ components/sections/about.jsx
@@ -63,30 +63,31 @@ const About = () => {
       </div>
 
       <section className="my-12">
-        <h1 className="text-3xl font-bold mb-8 text-white">What I Do</h1>
+        <div className="flex flex-col sm:flex-row items-center sm:items-start">
+          <h1 className="text-3xl font-bold mb-8 text-white pl-7">What I Do</h1>
+        </div>
+
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {/* First Card */}
           <div className="relative bg-gray-600 bg-opacity-20 backdrop-blur-md border border-gray-800 shadow-inner rounded-xl p-6">
-            <div className="flex items-start space-x-6">
+            <div className="flex flex-col sm:flex-row items-center sm:items-start space-y-4 sm:space-y-0 sm:space-x-6">
               {/* SVG Icon */}
-              <div className="w-40 h-20">
+              <div className="w-20 h-20 sm:w-40 sm:h-20">
                 <img
                   src="/images/icon-design.svg"
                   alt="Web Development"
                   className="w-full h-full object-contain"
                 />
               </div>
+
               {/* Title and Description */}
-              <div>
+              <div className="text-center sm:text-left">
                 <h2 className="text-2xl font-semibold text-white">
                   Software Engineer
                 </h2>
                 <p className="text-gray-300 mt-2">
-                  As a Software Engineer, I design, develop, and maintain
-                  software solutions, focusing on creating efficient, scalable,
-                  and secure systems. My work spans the full development
-                  lifecycle, ensuring high-quality software that meets both user
-                  and business needs.
+                  I design, develop, and maintain software solutions, ensuring
+                  efficiency, scalability, and security.
                 </p>
               </div>
             </div>
@@ -94,26 +95,24 @@ const About = () => {
 
           {/* Second Card */}
           <div className="relative bg-gray-600 bg-opacity-20 backdrop-blur-md border border-gray-800 shadow-inner rounded-xl p-6">
-            <div className="flex items-start space-x-6">
+            <div className="flex flex-col sm:flex-row items-center sm:items-start space-y-4 sm:space-y-0 sm:space-x-6">
               {/* SVG Icon */}
-              <div className="w-40 h-30">
+              <div className="w-20 h-20 sm:w-40 sm:h-20">
                 <img
                   src="/images/icon-dev.svg"
-                  alt="Mobile Development"
+                  alt="Full Stack Developer"
                   className="w-full h-full object-contain"
                 />
               </div>
+
               {/* Title and Description */}
-              <div>
+              <div className="text-center sm:text-left">
                 <h2 className="text-2xl font-semibold text-white">
                   Full Stack Web Developer
                 </h2>
                 <p className="text-gray-300 mt-2">
-                  As a Full Stack Web Developer, I build and maintain both
-                  frontend and backend systems, ensuring seamless functionality,
-                  scalability, and user-friendly experiences. I work with a
-                  range of technologies to deliver complete, secure, and
-                  efficient web solutions.
+                  I build and maintain frontend & backend systems, ensuring
+                  seamless functionality and great user experience.
                 </p>
               </div>
             </div>
@@ -122,9 +121,7 @@ const About = () => {
       </section>
 
       <section className="my-16 px-4 md:px-8">
-        <h1 className="text-3xl font-bold mb-12 text-center text-white">
-          Testimonials
-        </h1>
+        <h1 className="text-3xl font-bold mb-12 text-white">Testimonials</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
           {testimonials.map((testimonial, index) => (
             <div
